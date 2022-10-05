9bbbf2974edaafcfffdcd9cfb3aecb4acb9daa11
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-10-05 23:56:30 +0200

    added basic canvas component

---
+++ src/lib/Canvas.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { onMount } from "svelte";
+
+  export let width = 100;
+  export let height = 100;
+
+  let canvas;
+  let context;
+
+  onMount(() => {
+    context = canvas.getContext("2d");
+  });
+</script>
+
+<canvas bind:this={canvas} {width} {height} />
+
+<style>
+</style>
