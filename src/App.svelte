4811f4b62036f69c8cfc6c9f7375dd49b5f1124a
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-10-07 23:54:21 +0200

    basic setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,15 @@
 <script>
   import Canvas from "./lib/Canvas.svelte";
+  import Triangle from "./lib/Triangle.svelte";
+
+  let innerWidth;
+  let innerHeight;
 </script>
 
-<Canvas />
+<svelte:window bind:innerWidth bind:innerHeight />
+<Canvas let:props={cp} width={innerWidth} height={innerHeight}>
+  <Triangle props={cp} />
+</Canvas>
+
+<style>
+</style>
--- src/lib/Canvas.svelte
+++ src/lib/Canvas.svelte
@@ -1,18 +1,35 @@
 <script>
-  import { onMount } from "svelte";
+  import { onMount, setContext } from "svelte";
 
   export let width = 100;
   export let height = 100;
 
   let canvas;
   let context;
+  let props;
+
+  let key = Symbol();
+  let canvasObjects = [];
+  let animationFrame;
+
+  setContext(key, canvasObjects);
 
   onMount(() => {
     context = canvas.getContext("2d");
+    props = { context, test: "test" };
   });
+
+  function renderLoop() {
+    animationFrame = requestAnimationFrame(renderLoop);
+  }
 </script>
 
-<canvas bind:this={canvas} {width} {height} />
+<canvas bind:this={canvas} {width} {height}>
+  <slot {props} />
+</canvas>
 
 <style>
+  canvas {
+    display: block;
+  }
 </style>
