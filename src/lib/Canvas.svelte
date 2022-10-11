cb85f0391dc108ecb05cf741ae2a3f88751b56ec
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-10-11 23:53:31 +0200

    added check if entity mounted, moved props to loop

--- src/lib/Canvas.svelte
+++ src/lib/Canvas.svelte
@@ -8,7 +8,7 @@
   let canvas;
   let canvasBackground;
   let context;
-  let dt;
+  let dt = 0;
   let frameStart = performance.now();
   let props;
 
@@ -28,24 +28,23 @@
 
   onMount(() => {
     context = canvas.getContext("2d");
-
-    dt = 0;
-    props = { context, dt, width, height };
     renderLoop();
   });
 
   function renderLoop() {
     dt = (performance.now() - frameStart) / 1000;
-    props.dt = dt;
     frameStart = performance.now();
     animationFrame = requestAnimationFrame(renderLoop);
     context.clearRect(0, 0, width, height);
     context.fillStyle = backgroundColor;
     context.fillRect(0, 0, width, height);
+    props = { context, dt, width, height };
     canvasEntities.forEach((entity) => {
-      context.save();
-      entity.draw(props);
-      context.restore();
+      if (entity.mounted && entity.draw) {
+        context.save();
+        entity.draw(props);
+        context.restore();
+      }
     });
   }
 </script>
@@ -59,11 +58,5 @@
 <style>
   canvas {
     display: block;
-  }
-
-  #background {
-    position: fixed;
-    top: 0;
-    left: 0;
   }
 </style>
